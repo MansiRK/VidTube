742603e4ae1b39eb3b7f255914b7b9cc621a9b21
MansiRK <19302009mansi@viva-technology.org>
2023-07-07 06:51:08 +0530

    made single video card

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,6 +1,11 @@
 import React from "react"
 import styled from "styled-components"
-
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOffAltOutlinedIcon from "@mui/icons-material/ThumbDownOffAltOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import VideoCard from "../components/VideoCard";
+import Channel_Logo from "../image/channel_logo.jpeg"
 
 const Container = styled.div`
 display: flex;
@@ -20,15 +25,17 @@ const VideoWrapper = styled.div`
 const Title = styled.h1`
 
 font-size: 20px;
-font-weight: 500;
+font-weight: bold;
 margin-bottom: 10px;
 margin-top: 20px;
+margin-left: 5px;
 color: ${({theme}) => theme.text};
 
 `
 
 const Details = styled.div`
 display: flex;
+margin-left: 5px;
 align-itms: center;
 justify-content: space-between;
 `
@@ -40,23 +47,34 @@ color: ${({theme}) => theme.textSoft};
 const Button = styled.div`
 display: flex;
 align-item: center;
+padding: 5px;
 gap: 5px;
 cursor: pointer;
-padding: 5px 15px;
-background-color: transparent;
 border: 1px solid #ccc;
 color: ${({theme}) => theme.text};
 border-radius: 50px;
 `
+
 const Buttons = styled.div`
 display: flex;
 gap: 25px;
 color: ${({theme}) => theme.text};
 
 `
+const Subscribe = styled.button`
+  background-color: #cc1a00;
+  font-weight: 500;
+  color: white;
+  border: none;
+  border-radius: 3px;
+  height: max-content;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
 
 const Recommendations = styled.div`
 flex: 2;
+overflow: hidden;
 
 `
 
@@ -64,6 +82,29 @@ const Hr = styled.hr`
 margin: 15px 0px;
 border: 0.5px solid ${({theme}) => theme.soft}; 
 `
+
+const Channel = styled.div`
+display: flex;
+justify-content: space-between;
+ `
+const ChannelInfo = styled.div`
+display: flex;
+gap: 20px;
+`
+
+
+
+const ChannelLogo = styled.img`
+ width: 50px;
+ height: 50px;
+ border-radius: 50%;
+`;
+
+const ChannelName = styled.span`
+margin-top: 10px;
+  font-weight: 500;
+  color: ${({theme}) => theme.text};
+`;
 
 
 
@@ -84,27 +125,52 @@ const Video = () =>{
     </VideoWrapper>
     <Title>HTML Tutorial For Beginners</Title>
     <Details>
+    
         <Info>
             5M subscribers
         </Info>
         <Buttons>
             <Button>
-                Subscribe
+            <ThumbUpOutlinedIcon /> 
+                1.2K
             </Button>
             <Button>
-                Subscribe
+            <ThumbDownOffAltOutlinedIcon />
+            Dislikes
             </Button>
             <Button>
-                Subscribe
+            <ReplyOutlinedIcon /> Share
             </Button>
             <Button>
-                Subscribe
+                <FileDownloadOutlinedIcon/>
+                Download
             </Button>
         </Buttons>
     </Details>
     <Hr/>
+    <Channel>
+        <ChannelInfo>
+        <ChannelLogo src={Channel_Logo} />
+        <ChannelName>Code with Harry</ChannelName>
+        </ChannelInfo>
+        <Subscribe>Subscribe</Subscribe>
+    </Channel>
    </Content>
-   <Recommendations>Recommendations</Recommendations>
+   <Recommendations>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+        <VideoCard type="sm"/>
+      </Recommendations>
 </Container>
     )
 }
